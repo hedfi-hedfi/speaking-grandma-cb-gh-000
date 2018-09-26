6aa709b20975192676709e7e817307a303c18b30
Hamza Hedfi <h.hedfi@outlook.com>
2018-09-26 13:17:39 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -15,4 +15,7 @@ def speak_to_grandma(phrase)
   if(phrase != phrase.upcase)
     return "HUH?! SPEAK UP, SONNY!"
   end
+  if(phrase = phrase.upcase)
+    return "HUH?! SPEAK UP, SONNY!"
+  end
 end
